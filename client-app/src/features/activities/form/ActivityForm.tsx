5ac6ce6868ff46fc144cccc12572fbac0a187b61
Balakrishna Barre <balakrishna.b3@outlook.com>
2021-04-05 10:51:12 +0530

    End of section 6

--- client-app/src/features/activities/form/ActivityForm.tsx
+++ client-app/src/features/activities/form/ActivityForm.tsx
@@ -7,9 +7,10 @@ interface Props {
     activity: Activity | undefined;
     closeForm: () => void;
     createOrEdit: (activity: Activity) => void;
+    submitting: boolean;
 }
 
-export default function ActivityForm({activity: selectedActivity, closeForm, createOrEdit} : Props) {
+export default function ActivityForm({activity: selectedActivity, closeForm, createOrEdit, submitting} : Props) {
 
     const initialState = selectedActivity ??  { 
         id: '',
@@ -38,10 +39,10 @@ export default function ActivityForm({activity: selectedActivity, closeForm, cre
                 <Form.Input placeholder='Title' value={activity.title} name='title' onChange={hangleInputChange} />
                 <Form.TextArea placeholder='Description' value={activity.description} name='description' onChange={hangleInputChange}  />
                 <Form.Input placeholder='Category' value={activity.category} name='category' onChange={hangleInputChange}  />
-                <Form.Input placeholder='Date' value={activity.date} name='date' onChange={hangleInputChange}  />
+                <Form.Input type='date' placeholder='Date' value={activity.date} name='date' onChange={hangleInputChange}  />
                 <Form.Input placeholder='City' value={activity.city} name='city' onChange={hangleInputChange}  />
                 <Form.Input placeholder='Venue' value={activity.venue} name='venue' onChange={hangleInputChange}  />
-                <Button floated='right' positive type='submit'  content='Submit'/>
+                <Button loading={submitting} floated='right' positive type='submit' content='Submit'/>
                 <Button onClick={closeForm} floated='right' type='button' content='Cancel'/>
             </Form>
         </Segment>
