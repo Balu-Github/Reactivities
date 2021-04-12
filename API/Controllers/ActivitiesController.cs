f55902aecf7d9fde33eab901b5b2608ac1f63ebd
Balakrishna Barre <balakrishna.b3@outlook.com>
2021-04-12 10:35:51 +0530

    End of section 13

--- API/Controllers/ActivitiesController.cs
+++ API/Controllers/ActivitiesController.cs
@@ -7,7 +7,7 @@ using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace API.Controllers
-{
+{   
     public class ActivitiesController : BaseApiController
     {        
 
